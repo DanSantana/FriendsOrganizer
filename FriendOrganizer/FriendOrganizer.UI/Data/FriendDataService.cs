42b3252e6928624c84c7ea9efe29965362aad142
DanSantana <dangelosantana@hotmail.com>
2022-09-23 18:23:02 -0400

    inplementing PropertyChangedEventArgs INotifyPropertyChanged MainViewModel on FriendOrganizer.UI.ViewModel

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: later we will load from database
+            yield return new Friend { FirstName = "Thomas", LastName = "Turbano" };
+            yield return new Friend { FirstName = "Paula", LastName = "Dencro" };
+            yield return new Friend { FirstName = "Kiko", LastName = "Grande" };
+            yield return new Friend { FirstName = "Toku", LastName = "Tannavara" };
+        }
+    }
+}
---
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.ObjectModel;
+using FriendOrganizer.UI.Data;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel:INotifyPropertyChanged
+    {
+        private string _selectFriend;
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        private IFriendDataService _friendDataService;
+        //INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+
+        }
+             
+
+        public string SelectFriend
+        {
+            get { return _selectFriend; }
+            set
+            {
+                _selectFriend = value;
+                OnPropertyChange();
+            }
+        }
+
+        private void OnPropertyChange([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
